0ee8521aa33cd6e9be5d22dee308def0a34609dc
kanechu <itdept@itdepts-MacBook-Pro.local>
2014-10-17 20:49:28 -0700

    0

---
+++ itleo/NSArray.m
@@ -0,0 +1,44 @@
+//
+//  NSArray.m
+//  worldtrans
+//
+//  Created by itdept on 14-3-26.
+//  Copyright (c) 2014年 itdept. All rights reserved.
+//
+
+#import "NSArray.h"
+
+@implementation NSArray(ArrayWithObject)
++(NSArray *) arrayWithPropertiesOfObject:(id) obj{
+    NSMutableArray *arr=[NSMutableArray array];
+    unsigned count;
+    objc_property_t *properties = class_copyPropertyList([obj class], &count);
+    
+    for (int i = 0; i < count; i++) {
+        NSString *key = [NSString stringWithUTF8String:property_getName(properties[i])];
+        [arr addObject:key];
+    }
+    
+    free(properties);
+    
+    return [NSArray arrayWithArray:arr];
+}
++(NSArray *) arrayWithPropertiesValueOfObject:(id) obj{
+    NSMutableArray *arr=[NSMutableArray array];
+    unsigned count;
+    objc_property_t *properties = class_copyPropertyList([obj class], &count);
+    
+    for (int i = 0; i < count; i++) {
+        NSString *key = [NSString stringWithUTF8String:property_getName(properties[i])];
+        if ([obj valueForKey:key]==nil) {
+            [arr addObject:@""];
+        }else{
+            [arr addObject:[obj valueForKey:key]];
+        }
+       
+    }
+    free(properties);
+    return [NSArray arrayWithArray:arr];
+}
+
+@end
