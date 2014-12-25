ecfc5acc5b357a5b51160a003630abb9e55d819a
kanechu <kanechu@itdept.com.hk>
2014-12-25 17:20:31 +0800

    fix -save whs config

--- itleo/DB_whs_config.m
+++ itleo/DB_whs_config.m
@@ -32,43 +32,46 @@
         if ([db open]) {
             for (Resp_whs_config *whs_config in alist_result) {
                 NSArray *maintForm=whs_config.MaintForm;
-                NSInteger j=1;
+                NSInteger i=1;
                 for (Resp_MaintForm *resp_maint_obj in maintForm) {
                     NSMutableDictionary *dic_whs_header=[[NSDictionary dictionaryWithPropertiesOfObject:resp_maint_obj]mutableCopy];
                     [dic_whs_header removeObjectsForKeys:@[@"FORMAT_NAME",@"UPLOAD_COL"]];
                     NSArray *arr_format_names=resp_maint_obj.FORMAT_NAME;
                     for (Resp_language_type *language_obj in arr_format_names) {
-                        NSArray *arr_language_key=[NSArray arrayWithPropertiesOfObject:language_obj];
-                        NSArray *arr_language_value=[NSArray arrayWithPropertiesValueOfObject:language_obj];
-                        NSInteger i=0;
-                        for (NSString *key in arr_language_key) {
-                            NSString *key_value=[arr_language_value objectAtIndex:i];
-                            [dic_whs_header setObject:key_value forKey:key];
+                        NSDictionary *dic_language=[NSDictionary dictionaryWithPropertiesOfObject:language_obj];
+                        for (NSString *str_key in [dic_language allKeys]) {
+                            NSString *key_value=[dic_language valueForKey:str_key];
+                            [dic_whs_header setObject:key_value forKey:str_key];
                             key_value=nil;
-                            i++;
                         }
-                        arr_language_key=nil;
-                        arr_language_value=nil;
+                        dic_language=nil;
                         
                     }
                     NSArray *arr_upload_col=resp_maint_obj.UPLOAD_COL;
                     NSString *num=[NSString stringWithFormat:@"%d",[arr_upload_col count]];
                     [dic_whs_header setObject:num forKey:@"NUM"];
                     ib_updated=[db executeUpdate:@"insert into whs_config_header(EN,CN,TCN,ENABLE,UPLOAD_TYPE,NUM)values(:EN,:CN,:TCN,:ENABLE,:UPLOAD_TYPE,:NUM)" withParameterDictionary:dic_whs_header];
+                    dic_whs_header=nil;
+                    
                     for (Resp_UPLOAD_COL *upload_col_obj in arr_upload_col) {
                         NSMutableDictionary *dic_whs_detail=[[NSDictionary dictionaryWithPropertiesOfObject:upload_col_obj]mutableCopy];
                         [dic_whs_detail removeObjectForKey:@"col_label"];
-                        [dic_whs_detail setObject:[NSString stringWithFormat:@"%d",j] forKey:@"unique_id"];
+                        [dic_whs_detail setObject:[NSString stringWithFormat:@"%d",i] forKey:@"unique_id"];
                         NSArray *arr_col_labels=upload_col_obj.col_label;
-                        if ([arr_col_labels count]!=0) {
-                            Resp_language_type *col_label_type=[arr_col_labels objectAtIndex:0];
-                            [dic_whs_detail setObject:col_label_type.EN forKey:@"EN"];
-                            [dic_whs_detail setObject:col_label_type.CN forKey:@"CN"];
-                            [dic_whs_detail setObject:col_label_type.TCN forKey:@"TCN"];
-                         }
+                        for (Resp_language_type *language_obj in arr_col_labels) {
+                            NSDictionary *dic_language=[NSDictionary dictionaryWithPropertiesOfObject:language_obj];
+                            for (NSString *str_key in [dic_language allKeys]) {
+                                NSString *key_value=[dic_language valueForKey:str_key];
+                                [dic_whs_detail setObject:key_value forKey:str_key];
+                                key_value=nil;
+                            }
+                            dic_language=nil;
+                            
+                        }
                         ib_updated=[db executeUpdate:@"insert into whs_upload_col(seq,col_field,EN,CN,TCN,col_type,col_option,col_def,group_name,is_mandatory,unique_id)values(:seq,:col_field,:EN,:CN,:TCN,:col_type,:col_option,:col_def,:group_name,:is_mandatory,:unique_id)" withParameterDictionary:dic_whs_detail];
+                        dic_whs_detail=nil;
                     }
-                  j++;
+                  i++;
                 }
                 
             }
