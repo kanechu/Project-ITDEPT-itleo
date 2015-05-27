3712413a7f18ef848a96e32965d39ab86266cbe0
kanechu <kanechu@itdept.com.hk>
2015-05-27 15:23:34 +0800

    fix -change settings structure

--- itleo/EPODSettingsViewController.h
+++ itleo/EPODSettingsViewController.h
@@ -11,23 +11,4 @@
 
 @interface EPODSettingsViewController : UITableViewController
 
-@property (weak, nonatomic) IBOutlet Custom_BtnGraphicMixed *ibtn_setting_logo;
-
-@property (weak, nonatomic) IBOutlet UILabel *ilb_search_range;
-@property (weak, nonatomic) IBOutlet UILabel *ilb_date_range;
-
-@property (weak, nonatomic) IBOutlet UILabel *ilb_auto_range;
-@property (weak, nonatomic) IBOutlet UILabel *ilb_interval;
-
-@property (weak, nonatomic) IBOutlet UILabel *ilb_transfer;
-@property (weak, nonatomic) IBOutlet UISwitch *is_switch;
-
-@property (weak, nonatomic) IBOutlet UILabel *ilb_transfer_GPS;
-@property (weak, nonatomic) IBOutlet UISwitch *is_switch1;
-
-@property (weak, nonatomic) IBOutlet UILabel *ilb_record_GPS;
-@property (weak, nonatomic) IBOutlet UISwitch *is_switch2;
-
-@property (weak, nonatomic) IBOutlet UIButton *ibtn_back;
-
 @end
--- itleo/EPODSettingsViewController.m
+++ itleo/EPODSettingsViewController.m
@@ -9,14 +9,20 @@
 #import "EPODSettingsViewController.h"
 #import "RangeSelectViewController.h"
 #import "DB_sypara.h"
+#import "settings_dataModel.h"
+#define TABLE_HEADER_HEIGHT 40
+#define DEFAULT_TAG 100
 @interface EPODSettingsViewController ()
-@property (strong,nonatomic)NSMutableArray *alist_variate;
+@property (strong,nonatomic)NSMutableArray *alist_settings;
 
-@property (assign,nonatomic)NSInteger flag_range_type;
-//存储日期的key
-@property (copy, nonatomic)NSString *str_date_range;
-//存储时间的key
-@property (copy, nonatomic)NSString *str_interval_range;
+@property (weak, nonatomic) IBOutlet Custom_BtnGraphicMixed *ibtn_setting_logo;
+
+@property (weak, nonatomic) IBOutlet UIButton *ibtn_back;
+
+@property (assign,nonatomic)kRange_type range_type;
+
+@property (copy, nonatomic)NSString *str_select_range;
+
 @end
 
 @implementation EPODSettingsViewController
@@ -33,10 +39,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self fn_assignment_flag];
+    
     [self fn_set_control_pro];
+    [self fn_setExtraCellLineHidden];
+    [self fn_init_settings];
     //[self fn_isUsed_GPS_function];
     
+}
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:@"selected_range" object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(fn_update_range:) name:@"selected_range" object:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,62 +57,13 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
--(void)fn_assignment_flag{
-    NSUserDefaults *userDefaults=[NSUserDefaults standardUserDefaults];
-    NSString *date_key=[userDefaults objectForKey:@"date_range"];
-    if ([date_key length]!=0) {
-        _ilb_date_range.text=MY_LocalizedString(date_key, nil);
-        _str_date_range=date_key;
-    }else{
-        _str_date_range=@"lbl_day";
-        _ilb_date_range.text=MY_LocalizedString(_str_date_range, nil);
-        [userDefaults setObject:_str_date_range forKey:@"date_range"];
-    }
-    date_key=nil;
-    NSString *interval_key=[userDefaults objectForKey:@"interval_range"];
-    if ([interval_key length]!=0) {
-        _ilb_interval.text=MY_LocalizedString(interval_key, nil);
-        _str_interval_range=interval_key;
-        
-    }else{
-        _str_interval_range=@"lbl_minute";
-        _ilb_interval.text=MY_LocalizedString(_str_interval_range, nil);
-        [userDefaults setObject:_str_interval_range forKey:@"interval_range"];
-    }
-    interval_key=nil;
-}
+
 -(void)fn_set_control_pro{
     [_ibtn_setting_logo setTitle:MY_LocalizedString(@"ibtn_settings",nil) forState:UIControlStateNormal];
     [_ibtn_setting_logo setImage:[UIImage imageNamed:@"itdept_itleo"] forState:UIControlStateNormal];
-    _ilb_search_range.text=MY_LocalizedString(@"lbl_date_range", nil);
-    _ilb_auto_range.text=MY_LocalizedString(@"lbl_interval", nil);
-    _ilb_transfer.text=MY_LocalizedString(@"lbl_transfer_records", nil);
-    _ilb_transfer_GPS.text=MY_LocalizedString(@"lbl_transfer_GPS", nil);
-    _ilb_record_GPS.text=MY_LocalizedString(@"lbl_record_GPS", nil);
-    
-    _ibtn_back.layer.cornerRadius=4;
-    _ibtn_back.layer.borderColor=COLOR_light_BLUE.CGColor;
-    _ibtn_back.layer.borderWidth=1.5;
-    [_ibtn_back setTitle:MY_LocalizedString(@"lbl_back", nil) forState:UIControlStateNormal];
+    [_ibtn_back setTitle:MY_LocalizedString(@"lbl_cancel", nil) forState:UIControlStateNormal];
     [_ibtn_back addTarget:self action:@selector(fn_back_previous_page:)  forControlEvents:UIControlEventTouchUpInside];
-    
-    NSUserDefaults *userDefaults=[NSUserDefaults standardUserDefaults];
-    NSInteger _flag_transfer_record= [userDefaults integerForKey:@"transfer_record"];
-    NSInteger _flag_transfer_GPS=[userDefaults integerForKey:@"transfer_GPS"];
-    NSInteger _flag_record_GPS=[userDefaults integerForKey:@"record_GPS"];
-    [_is_switch addTarget:self action:@selector(fn_isAuto_transfer_data) forControlEvents:UIControlEventValueChanged];
-    if (_flag_transfer_record==0) {
-        _is_switch.on=NO;
-    }
-    
-    [_is_switch1 addTarget:self action:@selector(fn_isAuto_transfer_GPS) forControlEvents:UIControlEventValueChanged];
-    if (_flag_transfer_GPS==0) {
-        _is_switch1.on=NO;
-    }
-    [_is_switch2 addTarget:self action:@selector(fn_isRecord_GPS) forControlEvents:UIControlEventValueChanged];
-    if (_flag_record_GPS==0) {
-        _is_switch2.on=NO;
-    }
+  
     self.navigationItem.backBarButtonItem=[[UIBarButtonItem alloc] initWithTitle:MY_LocalizedString(@"ibtn_settings", nil)
                                                                            style:UIBarButtonItemStylePlain
                                                                           target:nil
@@ -107,50 +71,78 @@
     [self.navigationItem setHidesBackButton:YES];
     
 }
--(void)fn_isUsed_GPS_function{
-    DB_sypara *db_syparaObj=[[DB_sypara alloc]init];
-    if ([db_syparaObj fn_isExist_sypara_data:PARA_CODE_GPS data1:PARA_DATA1]) {
-        _is_switch1.enabled=NO;
-        _is_switch2.enabled=NO;
+- (void)fn_init_settings{
+    
+    NSMutableArray *alist_epod_settings=[NSMutableArray array];
+    
+    NSUserDefaults *userDefaults=[NSUserDefaults standardUserDefaults];
+    NSString *date_key=[userDefaults objectForKey:SETTINGS_DATE_RANGE];
+    if ([date_key length]==0) {
+        date_key=@"lbl_day";
+        [userDefaults setObject:date_key forKey:SETTINGS_DATE_RANGE];
     }
-}
-#pragma mark -event action
--(void)fn_back_previous_page:(id)sender{
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
--(void)fn_isAuto_transfer_data{
-    NSString *flag_result;
-    if (_is_switch.on) {
-        flag_result=@"1";
-    }else{
-        flag_result=@"0";
+    [alist_epod_settings addObject:[settings_dataModel fn_get_settings_dataModel:MY_LocalizedString(@"lbl_date_range", nil) interval:MY_LocalizedString(date_key, nil) type:SELECT_INTERVAL switch_isOn:NO]];
+    date_key=nil;
+    
+    NSString *interval_key=[userDefaults objectForKey:SETTINGS_ORDER_INTERVAL];
+    if ([interval_key length]==0) {
+        interval_key=@"lbl_minute";
+        [userDefaults setObject:interval_key forKey:SETTINGS_ORDER_INTERVAL];
     }
-    [self fn_define_userDefaults:flag_result key:@"transfer_record"];
-    [[NSNotificationCenter defaultCenter]postNotificationName:@"transfer_record" object:flag_result];
+    [alist_epod_settings addObject:[settings_dataModel fn_get_settings_dataModel:MY_LocalizedString(@"lbl_interval", nil) interval:MY_LocalizedString(interval_key, nil) type:SELECT_INTERVAL switch_isOn:NO]];
+    interval_key=nil;
+    
+    NSInteger _flag_transfer_record= [userDefaults integerForKey:@"transfer_record"];
+    BOOL switch_isOn=YES;
+    if (_flag_transfer_record==0) {
+        switch_isOn=NO;
+    }
+    [alist_epod_settings addObject:[settings_dataModel fn_get_settings_dataModel:MY_LocalizedString(@"lbl_transfer_records", nil) interval:nil type:SWITCH_FUNCTION switch_isOn:switch_isOn]];
+    
+    NSInteger _flag_transfer_GPS=[userDefaults integerForKey:@"transfer_GPS"];
+    BOOL gps_switch_isOn=YES;
+    if (_flag_transfer_GPS==0) {
+        gps_switch_isOn=NO;
+    }
+    [alist_epod_settings addObject:[settings_dataModel fn_get_settings_dataModel:MY_LocalizedString(@"lbl_record_andUpload_GPS", nil) interval:nil type:SWITCH_FUNCTION switch_isOn:gps_switch_isOn]];
+    
+    _alist_settings=[NSMutableArray arrayWithObject:alist_epod_settings];
+    
+    NSMutableArray  *alist_warehouse_settings=[NSMutableArray array];
+    NSString *whs_interval_key=[userDefaults objectForKey:SETTINGS_WHS_INTERVAL];
+    if ([whs_interval_key length]==0) {
+        whs_interval_key=@"lbl_minute";
+        [userDefaults setObject:whs_interval_key forKey:SETTINGS_WHS_INTERVAL];
+    }
+    [alist_warehouse_settings addObject:[settings_dataModel fn_get_settings_dataModel:MY_LocalizedString(@"lbl_upload_warehouse_interval", nil) interval:MY_LocalizedString(whs_interval_key, nil) type:SELECT_INTERVAL switch_isOn:NO]];
+    whs_interval_key=nil;
+    [self.alist_settings addObject:alist_warehouse_settings];
     
 }
--(void)fn_isAuto_transfer_GPS{
-    NSString* flag_result;
-    if (_is_switch1.on) {
-        flag_result=@"1";
-    }else{
-        flag_result=@"0";
-    }
-    [self fn_define_userDefaults:flag_result key:@"transfer_GPS"];
-    [[NSNotificationCenter defaultCenter]postNotificationName:@"transfer_GPS" object:flag_result];
+
+#pragma mark -event action
+
+- (IBAction)fn_back_previous_page:(id)sender { [self dismissViewControllerAnimated:YES completion:nil];
 }
--(void)fn_isRecord_GPS{
+
+-(void)fn_isAuto_transfer_data:(id)sender{
+    UISwitch *switchObj=(UISwitch*)sender;
+    NSInteger switch_tag=switchObj.tag-DEFAULT_TAG;
     NSString *flag_result;
-    if (_is_switch2.on) {
+    if (switchObj.on) {
         flag_result=@"1";
     }else{
         flag_result=@"0";
     }
-    [self fn_define_userDefaults:flag_result key:@"record_GPS"];
-    [[NSNotificationCenter defaultCenter]postNotificationName:@"record_GPS" object:flag_result];
-
+    if (switch_tag==2) {
+        [self fn_define_userDefaults:flag_result key:@"transfer_record"];
+        [[NSNotificationCenter defaultCenter]postNotificationName:@"transfer_record" object:flag_result];
+    }
+    if (switch_tag==3) {
+        [self fn_define_userDefaults:flag_result key:@"transfer_GPS"];
+        [[NSNotificationCenter defaultCenter]postNotificationName:@"transfer_GPS" object:flag_result];
+    }
 }
-
 -(void)fn_define_userDefaults:(NSString*)result key:(NSString*)key{
     NSUserDefaults *userDefaults=[NSUserDefaults standardUserDefaults];
     [userDefaults setObject:result forKey:key];
@@ -173,53 +165,99 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 3;
+    return [_alist_settings count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    if (section==0) {
-        return 2;
-    }
-    if (section==1) {
-        return 3;
-    }
-    if (section==2) {
-        return 1;
-    }
-    return 0;
+    return [[_alist_settings objectAtIndex:section]count];
 }
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellIdentifer=@"cell_settings";
+    UITableViewCell *cell=[self.tableView dequeueReusableCellWithIdentifier:cellIdentifer];
+    
+    NSMutableArray *alist_settings=[_alist_settings objectAtIndex:indexPath.section];
+    settings_dataModel *obj=[alist_settings objectAtIndex:indexPath.row];
+    cell.textLabel.text=obj.promptStr;
+    cell.detailTextLabel.text=obj.intervalStr;
+    NSInteger flag_type=obj.flag_settings_type;
+    if (flag_type==SWITCH_FUNCTION) {
+        UISwitch *switchObj=[[UISwitch alloc]init];
+        [switchObj addTarget:self action:@selector(fn_isAuto_transfer_data:) forControlEvents:UIControlEventValueChanged];
+        cell.accessoryView=switchObj;
+        switchObj.tag=DEFAULT_TAG+indexPath.row;
+        switchObj.on=obj.switch_isOn;
+    }
+    
+    return cell;
+}
+
 #pragma mark -UITableViewDelegate
+- (NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    if (section==0) {
+        return MY_LocalizedString(@"module_epod", nil);
+    }else if (section==1){
+       return MY_LocalizedString(@"module_warehouse", nil);
+    }
+    return nil;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+
+    return TABLE_HEADER_HEIGHT;
+}
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (indexPath.section==0) {
-        _flag_range_type=indexPath.row;
+    NSUserDefaults *userDefaults=[NSUserDefaults standardUserDefaults];
+    
+    NSMutableArray *alist_settings=[_alist_settings objectAtIndex:indexPath.section];
+    settings_dataModel *dataModel_obj=[alist_settings objectAtIndex:indexPath.row];
+    if (dataModel_obj.flag_settings_type==SELECT_INTERVAL) {
+        if ([dataModel_obj.promptStr isEqualToString:MY_LocalizedString(@"lbl_date_range", nil)]) {
+            _range_type=kDate_range;
+            _str_select_range=[userDefaults objectForKey:SETTINGS_DATE_RANGE];
+        }
+        if ([dataModel_obj.promptStr isEqualToString:MY_LocalizedString(@"lbl_interval", nil)]) {
+            _range_type=kOrder_interval_range;
+            _str_select_range=[userDefaults objectForKey:SETTINGS_ORDER_INTERVAL];
+        }
+        if ([dataModel_obj.promptStr isEqualToString:MY_LocalizedString(@"lbl_upload_warehouse_interval", nil)]) {
+            _range_type=kWhs_interval_range;
+            _str_select_range=[userDefaults objectForKey:SETTINGS_WHS_INTERVAL];
+        }
         [self performSegueWithIdentifier:@"Segue_RangeSelecte" sender:nil];
-        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(fn_update_range:) name:@"selected_range" object:nil];
-        
     }
 }
+-(void)fn_setExtraCellLineHidden{
+    UIView *view=[[UIView alloc]initWithFrame:CGRectZero];
+    view.backgroundColor=[UIColor clearColor];
+    [self.tableView setTableFooterView:view];
+    view=nil;
+}
+
 -(void)fn_update_range:(NSNotification*)notification{
     NSString *key_obj=(NSString*)[notification object];
-    if (_flag_range_type==0) {
-        _ilb_date_range.text=MY_LocalizedString(key_obj, nil);
-        [self fn_define_userDefaults:key_obj key:@"date_range"];
-        _str_date_range=key_obj;
-    }else if(_flag_range_type==1){
-        _ilb_interval.text=MY_LocalizedString(key_obj, nil);
-        [self fn_define_userDefaults:key_obj key:@"interval_range"];
-        _str_interval_range=key_obj;
+    if (_range_type==kDate_range) {
+        
+        [self fn_define_userDefaults:key_obj key:SETTINGS_DATE_RANGE];
+        
+    }else if(_range_type==kOrder_interval_range){
+        
+        [self fn_define_userDefaults:key_obj key:SETTINGS_ORDER_INTERVAL];
+        
+    }else if(_range_type==kWhs_interval_range){
+        
+        [self fn_define_userDefaults:key_obj key:SETTINGS_WHS_INTERVAL];
     }
+    
+    [self fn_init_settings];
+    [self.tableView reloadData];
 }
 #pragma mark - push must action method
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
     RangeSelectViewController *VC=(RangeSelectViewController*)[segue destinationViewController];
-    if (_flag_range_type==0) {
-        VC.str_range=_str_date_range;
-    }else{
-        VC.str_range=_str_interval_range;
-    }
-    VC.flag_range_type=_flag_range_type;
+    VC.str_range=_str_select_range;
+    VC.range_type=_range_type;
 }
 
 @end
